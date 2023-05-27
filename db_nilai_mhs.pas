df543b192a4a665bf22f14717f66568c1aa5335a
Rayhan Faridh <rayhanfrdh123@gmail.com>
2023-05-27 22:28:51 +0700

    tugas

--- db_nilai_mhs.pas
+++ db_nilai_mhs.pas
@@ -5,34 +5,88 @@ type
 	mhs = record
 		nim: string[9];
 		nama: string[30];
-		kelas: string[4];
-		nilai: smallint;
+		alamat: string[25];
+		nilai: record
+			tugas, uts, uas, prak: smallint;
+			akhir: real;
+		end;
+		grade: string[2];
 	end;
 
 var
   dataMhs: array of mhs;
-	i,n: integer;
+	i,j,n: integer;
 
+procedure nilai_akhir(); forward;
 procedure input();
+var batasJ, awal_I, akhir_I: integer;
 begin
-	clrscr;
-	write('Masukkan banyak mahasiswa: '); readln(n);
+	repeat
+		clrscr;
+		write('Masukkan banyak mahasiswa: '); readln(n);
+	until (n>0);
 	setlength(dataMhs, n);
 	
+	batasJ := n div 5; // untuk n lebih dari sama dengan 5
+	awal_I := 1;
+	akhir_I := 5;
+
+	if (n mod 5 <> 0) then batasJ := batasJ+1
+	else if (n<5) then batasJ := 1;
+
 	writeln;
-	for i:=0 to n-1 do begin
-		writeln('==========================');
-		writeln('Input data mahasiswa ke-',i+1);
-		with dataMhs[i] do
-		begin
-			write('NIM: '); readln(nim);
-			write('Nama: '); readln(nama);
-			write('Kelas: '); readln(kelas);
-			write('Nilai Alpro: '); readln(nilai);
+	for j:=1 to batasJ do begin
+		if j>1 then clrscr;
+		if (n>5) and (n mod 5<>0) and (j=batasJ) then akhir_I := n
+		else akhir_I := n;
+
+		for i:=awal_I to akhir_I do begin
+			writeln('==========================');
+			writeln('Input data mahasiswa ke-',i);
+			with dataMhs[i-1] do
+			begin
+				write('NIM: '); readln(nim);
+				write('Nama: '); readln(nama);
+				write('Alamat: '); readln(alamat);
+				with nilai do begin
+					write('Nilai Tugas: '); readln(tugas);
+					write('Nilai UTS: '); readln(uts);
+					write('Nilai UAS: '); readln(uas);
+					write('Nilai PRAK: '); readln(prak);
+				end;
+			end;
+			writeln('==========================');
+		end;
+		awal_I := i+1;
+		akhir_I := awal_I+4;
+		if j<batasJ then begin
+			write('Enter untuk melanjutkan input data '); readkey;
 		end;
 	end;
-	writeln('==========================');
-	writeln('Data sebanyak ', n, ' mahasiswa berhasil diinputkan.');
+	nilai_akhir();
+end;
+
+procedure nilai_akhir();
+begin
+	for i:=1 to n do begin
+		with dataMhs[i-1] do begin
+			with nilai do begin
+				akhir := (10/100)*tugas + (30/100)*uts + (30/100)*uas + (30/100)*prak;
+
+				case round(akhir) of
+					85..100: grade:='A';
+					80..84: grade:='A-';
+					75..79: grade:='B+';
+					70..74: grade:='B';
+					65..69: grade:='C+';
+					60..64: grade:='C';
+					55..59: grade:='D+';
+					40..54: grade:='D';
+					0..39: grade:='E';
+				end;
+			end;
+		end;
+	end;
 end;
 
 procedure tampil();
@@ -41,24 +95,7 @@ begin
 	clrscr;
 	writeln('Menampilkan data yang sudah diinputkan');
 	writeln('Data Mahasiswa Polstat STIS');
-	writeln('====================================================================');
-	writeln('|    NIM    |              Nama              | Kelas | Nilai Alpro |');
-	writeln('|-----------|--------------------------------|-------|-------------|');
-	for i:=0 to n-1 do
-	begin
-		with dataMhs[i] do begin
-			lbr1 := 9-length(nim);
-			lbr2 := 30-length(nama);
-			lbr3 := 5-length(kelas);
-			lbr4 := 11-length(intToStr(nilai));
-
-			write('| ', nim, '':lbr1, ' | ');
-			write(nama, '':lbr2, ' | ');
-			write(kelas, '':lbr3, ' | ');
-			writeln(nilai, '':lbr4, ' |');
-		end;
-	end;
-	writeln('====================================================================');
+	
 end;
 
 function max(): integer;
@@ -111,9 +148,6 @@ begin
 	case pil of
 		1: input();
     2: tampil();
-    3: writeln('Nilai Alpro terbesar adalah ', max());
-    4: writeln('Nilai Alpro terkecil adalah ', min());
-		5: writeln('Rata-rata nilai Alpro adalah ', rerata():0:2);
     else writeln('Terima kasih!');
 	end;
 	readkey;
